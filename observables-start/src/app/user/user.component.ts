a2891eef16750dd0581c9f1a88e3eb333a2ec933
saulrb <saulrb@hotmail.com>
2017-11-27 08:21:22 -0600

    Observables training code

---
+++ observables-start/src/app/user/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+  id: number;
+
+  constructor(private route: ActivatedRoute) { }
+  // Observables support three parameters one for data, error, and complete events.
+  ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+        },
+        () => {
+
+        },
+        () => {
+
+        }
+      );
+  }
+
+}
